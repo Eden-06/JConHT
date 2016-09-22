f0cbda8ae29ecd95f87f43a74ccf27ae82339d11
Stephan Boehme <boehme@tcs.inf.tu-dresden.de>
2016-09-22 10:25:25 +0200

    Initial commit

---
+++ src/main/java/de/tudresden/inf/lat/jconht/Main.java
@@ -0,0 +1,9 @@
+package de.tudresden.inf.lat.jconht;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Welcome to JConHT!");
+    }
+}
