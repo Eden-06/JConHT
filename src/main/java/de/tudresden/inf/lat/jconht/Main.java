ebd48753e3398509781fb31ec15a78ed4bbf6efa
Stephan Boehme <boehme@tcs.inf.tu-dresden.de>
2016-09-22 12:39:25 +0200

    Started with owlapi test

--- src/main/java/de/tudresden/inf/lat/jconht/Main.java
+++ src/main/java/de/tudresden/inf/lat/jconht/Main.java
@@ -1,9 +1,26 @@
 package de.tudresden.inf.lat.jconht;
 
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Welcome to JConHT!");
+
+        String inputDir = new File("input").getAbsolutePath();
+        IRI iri1 = IRI.create("file://" + inputDir + "/" + "hermit-testonto-1.owl");
+        IRI iri2 = IRI.create("file://" + inputDir + "/" + "pizza.owl");
+        OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+        //OWLOntology onto = manager.loadOntology(iri2);
+
+
+
+        System.out.println(inputDir);
     }
 }
