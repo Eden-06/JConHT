d1155cf2efbb659cc88597267ff22f985d0e821c
Marcel Lippmann <marcel.lippmann@tu-dresden.de>
2016-10-05 13:55:43 +0200

    Decorated Tableau instance.

--- src/main/java/de/tudresden/inf/lat/jconht/tableau/ContextReasoner.java
+++ src/main/java/de/tudresden/inf/lat/jconht/tableau/ContextReasoner.java
@@ -3,6 +3,7 @@ package de.tudresden.inf.lat.jconht.tableau;
 import org.semanticweb.HermiT.Configuration;
 import org.semanticweb.HermiT.Reasoner;
 import org.semanticweb.HermiT.model.DescriptionGraph;
+import org.semanticweb.HermiT.tableau.Tableau;
 import org.semanticweb.owlapi.model.OWLOntology;
 
 import java.util.Collection;
@@ -11,12 +12,11 @@ import java.util.Collection;
  * Created by boehme on 23/09/16.
  */
 public class ContextReasoner extends Reasoner {
-    public ContextReasoner(Configuration configuration, OWLOntology rootOntology, Collection<DescriptionGraph> descriptionGraphs) {
-        super(configuration, rootOntology, descriptionGraphs);
-    }
 
     public ContextReasoner(Configuration configuration, OWLOntology rootOntology) {
         super(configuration, rootOntology);
+
+        this.m_tableau = new ContextTableau(this.getTableau(), configuration);
     }
 
 }
--- src/main/java/de/tudresden/inf/lat/jconht/tableau/ContextTableau.java
+++ src/main/java/de/tudresden/inf/lat/jconht/tableau/ContextTableau.java
@@ -1,28 +1,34 @@
 package de.tudresden.inf.lat.jconht.tableau;
 
+import org.semanticweb.HermiT.Configuration;
 import org.semanticweb.HermiT.existentials.ExistentialExpansionStrategy;
+import org.semanticweb.HermiT.model.Atom;
 import org.semanticweb.HermiT.model.DLOntology;
+import org.semanticweb.HermiT.model.Individual;
+import org.semanticweb.HermiT.model.Term;
 import org.semanticweb.HermiT.monitor.TableauMonitor;
 import org.semanticweb.HermiT.tableau.InterruptFlag;
+import org.semanticweb.HermiT.tableau.Node;
+import org.semanticweb.HermiT.tableau.ReasoningTaskDescription;
 import org.semanticweb.HermiT.tableau.Tableau;
 
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by boehme on 23/09/16.
  */
 public class ContextTableau  extends Tableau{
 
-    /**
-     * @param interruptFlag                 interruptFlag
-     * @param tableauMonitor                tableauMonitor
-     * @param existentialsExpansionStrategy existentialsExpansionStrategy
-     * @param useDisjunctionLearning        useDisjunctionLearning
-     * @param permanentDLOntology           permanentDLOntology
-     * @param additionalDLOntology          additionalDLOntology
-     * @param parameters                    parameters
-     */
-    public ContextTableau(InterruptFlag interruptFlag, TableauMonitor tableauMonitor, ExistentialExpansionStrategy existentialsExpansionStrategy, boolean useDisjunctionLearning, DLOntology permanentDLOntology, DLOntology additionalDLOntology, Map<String, Object> parameters) {
-        super(interruptFlag, tableauMonitor, existentialsExpansionStrategy, useDisjunctionLearning, permanentDLOntology, additionalDLOntology, parameters);
+
+    public ContextTableau(Tableau tableau, Configuration configuration) {
+
+        super(tableau.getInterruptFlag(),
+                tableau.getTableauMonitor(),
+                tableau.getExistentialsExpansionStrategy(),
+                configuration.useDisjunctionLearning,
+                tableau.getPermanentDLOntology(),
+                tableau.getAdditionalDLOntology(),
+                configuration.parameters);
     }
 }
