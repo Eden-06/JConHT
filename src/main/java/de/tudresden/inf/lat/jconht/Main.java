21bdb4de8a3f258bfe533ba18fe9f82cde2a5eae
Stephan Boehme <stephan.boehme@tu-dresden.de>
2017-04-13 18:45:05 +0200

    changed Main to make it usable in the jar-file

--- src/main/java/de/tudresden/inf/lat/jconht/Main.java
+++ src/main/java/de/tudresden/inf/lat/jconht/Main.java
@@ -1,14 +1,17 @@
 package de.tudresden.inf.lat.jconht;
 
+import de.tudresden.inf.lat.jconht.model.Configuration;
 import de.tudresden.inf.lat.jconht.model.ContextOntology;
 import de.tudresden.inf.lat.jconht.tableau.ContextReasoner;
 import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.formats.ManchesterSyntaxDocumentFormatFactory;
+import org.semanticweb.owlapi.io.FileDocumentSource;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 
 import java.io.File;
+import java.util.Arrays;
 
 public class Main {
 
@@ -23,38 +26,49 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to JConHT!");
+        boolean verbose = Arrays.asList(args).contains("-v");
+
+        if (verbose) {
+            System.out.println("Welcome to JConHT!");
+        }
 
         File file = new File("input/hermit-testonto-2.owl");
         if (args.length != 0) {
-            switch (args[0]) {
-                case "framed":
-                    file = new File("input/Bank.owl");
-                    break;
-                default:
-                    file = new File("input/hermit-testonto-" + args[0] + ".owl");
-            }
+            file = new File(args[0]);
         }
 
         OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-
+        Configuration confWithoutDebug = new Configuration(true, false, false, false);
+        Configuration confWithDebug = new Configuration(true, true, false, false);
+        if (verbose) {
+            System.out.println("Loading " + file);
+            System.out.println();
+        }
         try {
 
             //TODO oder hier lieber manager.loadOntologyFromOntologyDocument(file) ?
-            OWLOntology onto = manager.loadOntology(IRI.create(file));
-            
-            ContextOntology contextOntology = new ContextOntology(onto);
-            System.out.println(contextOntology);
-            System.out.println("---------------------------------------------");
+            //OWLOntology rootOntology = manager.loadOntology(IRI.create(file));
 
-//            Debugger debugger = new Debugger(Prefixes.STANDARD_PREFIXES,true);
-//            confWithoutTableauMonitor.monitor = debugger;
+            FileDocumentSource fileDocumentSource = new FileDocumentSource(file, new ManchesterSyntaxDocumentFormatFactory().createFormat());
+            OWLOntology rootOntology = manager.loadOntologyFromOntologyDocument(fileDocumentSource);
+
+            ContextOntology contextOntology = verbose ?
+                    new ContextOntology(rootOntology, confWithDebug) :
+                    new ContextOntology(rootOntology, confWithoutDebug);
+            if (verbose) {
+                System.out.println(contextOntology);
+                System.out.println("---------------------------------------------");
+            }
+
             ContextReasoner reasoner = new ContextReasoner(contextOntology);
-            System.out.println("reasoner.getDLOntology().getDLClauses() = ");
-            reasoner.getDLOntology().getDLClauses().stream().forEach(System.out::println);
-            System.out.println("reasoner.getDLOntology().getPositiveFacts() = " + reasoner.getDLOntology().getPositiveFacts());
-            System.out.println("reasoner.getDLOntology().getNegativeFacts() = " + reasoner.getDLOntology().getNegativeFacts());
-            System.out.println("---------------------------------------------");
+            if (verbose) {
+                System.out.println("reasoner.getDLOntology().getDLClauses() = ");
+                reasoner.getDLOntology().getDLClauses().forEach(System.out::println);
+                System.out.println("reasoner.getDLOntology().getPositiveFacts() = " + reasoner.getDLOntology().getPositiveFacts());
+                System.out.println("reasoner.getDLOntology().getNegativeFacts() = " + reasoner.getDLOntology().getNegativeFacts());
+                System.out.println("---------------------------------------------");
+            }
+
             boolean result = reasoner.isConsistent();
 
             System.out.println(result);
